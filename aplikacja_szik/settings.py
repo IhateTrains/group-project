a2962f63a5dfcb6dc69cda7556e71488ccdd9321
IlikeTrains <sewerynpresnal@gmail.com>
2021-04-20 19:39:21 +0200

    settings for Heroku

--- aplikacja_szik/settings.py
+++ aplikacja_szik/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +144,7 @@ AUTHENTICATION_BACKENDS = (
 # media files
 MEDIA_ROOT = ''
 MEDIA_URL = "media/"
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
