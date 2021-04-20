3de870937e4155b3df8608519fcb7d0c77c65fb4
IlikeTrains <sewerynpresnal@gmail.com>
2021-04-20 20:31:24 +0200

    Update settings.py

--- aplikacja_szik/settings.py
+++ aplikacja_szik/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'aplikacja_szik.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
