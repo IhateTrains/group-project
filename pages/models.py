51d8de1435e7882e6ea77484f7cdbebcabbc535e
IlikeTrains <sewerynpresnal@gmail.com>
2021-04-21 21:37:15 +0200

    Update models.py

--- pages/models.py
+++ pages/models.py
@@ -18,7 +18,7 @@ class Category(models.Model):
 
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    categoryID = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     discountPrice = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
@@ -45,18 +45,42 @@ class Product(models.Model):
         })
 
 
+class ProductQuantity(models.Model):
+    productID = models.ForeignKey(Product, on_delete=models.CASCADE)
+    pointID = models.ForeignKey(SzikPoint, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+
+
 class OrderLine(models.Model):
-    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # userID
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
-    ordered = models.BooleanField(default=False)
+    ordered = models.BooleanField(default=False)  # orderStatus
 
     def __str__(self):
         return f"{self.quantity} x {self.product.name}"
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # userID
+    paymentMethod = models.CharField(max_length=60)
     products = models.ManyToManyField(OrderLine)
     orderDate = models.DateTimeField()
     ordered = models.BooleanField(default=False)
+
+
+class UserProfile(models.Model):
+    userID = models.OneToOneField(settings.AUTH_USER_MODEL)
+    telephone = models.CharField(max_length=9)
+    city = models.ImageField(max_length=30)
+    streetAddress = models.ImageField(max_length=50)
+    postalCode = models.ImageField(max_length=20)
+
+
+class UserType(models.Model):
+    name = models.CharField(max_length=20, null=False)
+
+
+class VinNumber(models.Model):
+    userID = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    vin = models.CharField(max_length=17)
