8dd4b7eebb3c7a6436a7c160d1543c1952e2e9cf
IlikeTrains <sewerynpresnal@gmail.com>
2021-04-21 18:46:46 +0200

    Added Category model

--- pages/models.py
+++ pages/models.py
@@ -5,7 +5,12 @@ from django.shortcuts import reverse
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     discountPrice = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
