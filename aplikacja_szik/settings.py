37e96e6b3e934d4cfd34f9f74d11933db21ddbf0
IlikeTrains <sewerynpresnal@gmail.com>
2021-04-28 00:09:39 +0200

    updated login template

--- aplikacja_szik/settings.py
+++ aplikacja_szik/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"), '/static/',
+]
 
 # Authentication
 AUTHENTICATION_BACKENDS = (
