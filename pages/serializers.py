8c975d27196bb88a5fb5a775c83d0f973cb9690e
IlikeTrains <sewerynpresnal@gmail.com>
2021-05-13 00:49:19 +0200

    Nearest shop finde

--- pages/serializers.py
+++ pages/serializers.py
@@ -1,1 +1,8 @@
 from rest_framework import serializers
+from .models import SzikPoint
+
+
+class SzikPointSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SzikPoint
+        fields = ['id', 'city', 'mapLatitude', 'mapLongitude']
--- pages/urls.py
+++ pages/urls.py
@@ -8,6 +8,7 @@ from .views import (
     ProductView,
     OrderSummaryView,
     ShopsView,
+    get_nearest_shop,
     add_to_cart,
     remove_from_cart,
     reduce_quantity_item,
@@ -19,6 +20,7 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('sales', SalesView.as_view(), name='sales'),
     path('shops', ShopsView.as_view(), name='shops'),
+    path('get-nearest-shop/<str:lat>/<str:lng>/', get_nearest_shop, name='get-nearest-shop'),
     path('about/', views.about, name='about'),
     path('product/<pk>/', ProductView.as_view(), name='product'),
     path('add-to-cart/<pk>/', add_to_cart, name='add-to-cart'),
@@ -26,5 +28,5 @@ urlpatterns = [
     path('reduce-quantity-item/<pk>/', reduce_quantity_item, name='reduce-quantity-item'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('checkout/', HomeView.as_view(), name='checkout'),  # TODO: add CheckOutView
-    path('register/',views.registerView, name='register'),
+    path('register/', views.registerView, name='register'),
 ]
