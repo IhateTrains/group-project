1626112fe277fb5747812e05112ea510a1b8fdf0
IhateTrains <IhateTrains@users.noreply.github.com>
2021-05-25 12:31:22 +0200

    Improve populate.py (#29)

--- pages/management/commands/populate.py
+++ pages/management/commands/populate.py
@@ -10,7 +10,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if models.Product.objects.count() < 2000:
             for cat_name in ('Silniki', 'Zderzaki', 'Reflektory', 'Płyny'):
-                baker.make('pages.Product', category__name=cat_name, _quantity=150)
+                new_category = baker.make('pages.Category', name=cat_name)
+                baker.make('pages.Product', category_id=new_category.pk, _quantity=150)
             self.stdout.write(self.style.SUCCESS('Successfully populated Products'))
         if models.SzikPoint.objects.count() < 50:
             baker.make('pages.SzikPoint', _quantity=50, _create_files=True)
