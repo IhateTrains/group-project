09e11d889a0e694a4471328736760795d18a458c
IlikeTrains <sewerynpresnal@gmail.com>
2021-05-25 09:04:36 +0200

    Update populate.py

--- pages/management/commands/populate.py
+++ pages/management/commands/populate.py
@@ -1,12 +1,15 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from model_bakery import baker
+from ... import models
 
 
 class Command(BaseCommand):
     help = 'Populates the database with random data'
 
     def handle(self, *args, **options):
-        baker.make('pages.Product', _quantity=1000)
-        baker.make('pages.Category', _quantity=20)
-        baker.make('pages.SzikPoint', _quantity=50)
+        if models.Product.objects.count < 1000:
+            baker.make('pages.Product', _quantity=1000)
+            baker.make('pages.Category', _quantity=20)
+        if models.SzikPoint.objects.count < 50:
+            baker.make('pages.SzikPoint', _quantity=50)
