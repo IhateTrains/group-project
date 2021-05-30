33ae860e1f35faa9ca2dbe4c9a8283db693d3a09
IlikeTrains <sewerynpresnal@gmail.com>
2021-05-30 22:11:55 +0200

    Tweak

--- pages/views.py
+++ pages/views.py
@@ -21,8 +21,6 @@ from django.contrib.auth.models import User
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
 from django.core.mail import EmailMessage
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
