9e82da34560ac8aaa7442228007070fd510a70ec
IlikeTrains <sewerynpresnal@gmail.com>
2021-06-02 00:56:11 +0200

    Update comparison.js

--- comparison/static/comparison/comparison.js
+++ comparison/static/comparison/comparison.js
@@ -1,8 +1,7 @@
-(function ($) {
-
-    ComparisonButton = function ($btn) {
-
+$(function () {
+    ComparisonButton = function () {
         var self = this;
+        var $btn = $('#compare_btn');
 
         this._toggleLoading = function (isVisible) {
             $btn.find('[data-role=loading]')[isVisible ? 'show' : 'hide']();
@@ -19,17 +18,18 @@
         this._toggleComparison($btn.data('active'));
 
         $btn.click(function () {
-            $btn.prop('disabled', true);
+            $('#compare_btn').prop('disabled', true);
             self._toggleLoading(true);
 
             $.post($btn.data('url'), function (response) {
                 self._toggleLoading(false);
-                $btn.prop('disabled', false);
+                $('#compare_btn').prop('disabled', false);
                 self._toggleComparison(response.is_active);
                 $('[data-role=comparison-dropdown]').html(response.dropdown);
                 $.notify({message: response.message}, {type: 'success'});
             });
         });
-    }
+    };
 
-})(jQuery);
+    ComparisonButton();
+});
