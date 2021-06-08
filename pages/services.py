b3f7977456f4fd119a907d1c36ad237218f1d782
kreedyX <55585560+kreedyX@users.noreply.github.com>
2021-06-08 21:37:29 +0200

    home view database functions moved to services

--- pages/services.py
+++ pages/services.py
@@ -1,6 +1,8 @@
 from django.db.models import Q
-from .models import SzikPoint, Product
+from .models import SzikPoint, Product, Category
 from .serializers import SzikPointSerializer
+from django.core.paginator import Paginator
+from .filters import ProductFilter
 
 
 def get_nearest_shop_data(lat, lng):
@@ -33,3 +35,46 @@ def get_nearest_shop_data(lat, lng):
 def get_matching_products(searched):
     return Product.objects.filter(Q(name__icontains=searched)
                                   | Q(description__icontains=searched))
+
+def get_products_and_categories(request, on_sale=False):
+    context = { 'section': None }
+    selected_category_pk = request.GET.get('category')
+    if selected_category_pk is not None and len(selected_category_pk) > 0:
+        
+        selected_category = Category.objects.get(pk=selected_category_pk)
+
+        if selected_category.parent_category is not None:
+            section_pk = selected_category.parent_category.pk
+            product_list = Product.objects.filter(category=selected_category_pk)
+        else:
+            section_pk = selected_category.pk
+            product_list = Product.objects.filter(category__parent_category=section_pk)
+            product_list |= Product.objects.filter(category=section_pk)
+
+        context['categories'] = Category.objects.filter(parent_category=section_pk)
+    else:
+        section_pk = None
+        context['categories'] = Category.objects.filter(parent_category__isnull=True)
+        product_list = Product.objects.all()
+
+    if section_pk is not None:
+        context['section'] = Category.objects.get(pk=section_pk)
+
+    if on_sale:
+        product_list = product_list.filter(discount__isnull=False)
+    
+    context['product_list'] = product_list
+    return context
+
+def get_product_list_page(request, product_list):
+
+    product_list = product_list.order_by('category__parent_category', 'category')
+    product_filter = ProductFilter(request.GET, queryset=product_list)
+    paginator = Paginator(product_filter.qs, 12)
+    page_number = request.GET.get('page')
+
+    if page_number is None or len(page_number) == 0:
+        page_number = 1
+
+    return paginator.get_page(page_number)
+    
